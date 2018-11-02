1734bf868ada078890d5103953ecdd4040875a5e
Chris Rollings <rollingc@coventry.ac.uk>
2018-11-02 13:15:37 +0000

    Week 3 update

---
+++ M19COM/Week3/workshop/NetbeansApplication/src/netbeansapplication/NetbeansApplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeansapplication;
+/**
+ *
+ * @author rollingc
+ */
+public class NetbeansApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BankAccountDemo.RunDemo(args);
+    }
+    
+}
